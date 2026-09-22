712e0e0e1c92a7317ee8f59cbcfb82c5e39f22a7
Mayaman-prog <np03cy4s250006@heraldcollege.edu.np>
2026-09-22 18:02:05 +0545

    Expose evidence correlation API

---
+++ src/evidence_correlation/__init__.py
@@ -0,0 +1,19 @@
+from src.evidence_correlation.correlator import (
+    AgreementState,
+    CorrelationDecision,
+    CorrelationEngine,
+    DecisionType,
+    Incident,
+    MatchType,
+)
+from src.evidence_correlation.policy import CorrelationPolicy
+
+__all__ = [
+    "AgreementState",
+    "CorrelationDecision",
+    "CorrelationEngine",
+    "CorrelationPolicy",
+    "DecisionType",
+    "Incident",
+    "MatchType",
+]
